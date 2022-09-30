78c79c6f64bc61b3c2d6a30df9a3e3017175eb90
ef <efdevfr@gmail.com>
2022-09-30 13:02:48 +0200

    -

--- src/components/globe/Globe.js
+++ src/components/globe/Globe.js
@@ -29,14 +29,6 @@ const Globe = forwardRef( (props, ref) => {
   const svgRef = useRef(null);
   const svg = d3.select(svgRef.current);
 
-  // Setup outerspace
-  // const space = d3.geoAzimuthalEquidistant()
-  //     .translate([dimensions.width / 2, dimensions.height / 2]);
-  // space.scale(space.scale() * 4);
-  // const spacePath = d3.geoPath()
-  //     .projection(space)
-  //     .pointRadius(1);
-
   // Cleanup svg before redraw
   svg.selectAll("*").remove();
 
@@ -62,7 +54,6 @@ const Globe = forwardRef( (props, ref) => {
       // .attr("height", '100vh')
       .attr("clip-path", "url(#clip)")
       .attr("d", path);
-  //console.log('path', path);
 
   // Build Globe (oceans)
   svg.append("path")
@@ -85,23 +76,9 @@ const Globe = forwardRef( (props, ref) => {
   const cancerTropicCoord = { W: -180.0, N: 23.43, E: 180.0, S: 23.43 };
   const cancerTropic = d3.geoGraticule().extentMajor([[cancerTropicCoord.W, cancerTropicCoord.S], [cancerTropicCoord.E, cancerTropicCoord.N]]);
   svg.append("path").attr("class", "cancerTropic").attr("clip-path", "url(#clip)").attr("d", path(cancerTropic.outline()));
-  // svg.append("path")
-  //   .datum({type: "LineString", coordinates: [
-  //     [-180, cancerTropicCoord.N], [-90, cancerTropicCoord.N], 
-  //     [0, cancerTropicCoord.N], 
-  //     [90, cancerTropicCoord.N], [180, cancerTropicCoord.N]
-  //   ]})
-  //   .attr("class", "cancerTropic")
-  //   .attr("d", path);
-  // Cancer Tropic Label
-  // svg.append("text").attr("class", "label tropic").text("Cancer Tropic")
-  //                   .attr("x", projection([0, 10])[1])
-  //                   .attr("y", projection([0, 25])[1]);
 
   // Equator
   const equatorCoord = { W: -180.0, N: 0, E: 180.0, S: 0 };
-  //const equator = d3.geoGraticule().extentMajor([[equatorCoord.W, equatorCoord.S], [equatorCoord.E, equatorCoord.N]]);
-  //svg.append("path").attr("class", "equator").attr("d", path(equator.outline()));
   svg.append("path")
     .datum({type: "LineString", coordinates: [
       [-180, equatorCoord.N], [-90, equatorCoord.N], 
@@ -111,29 +88,16 @@ const Globe = forwardRef( (props, ref) => {
     .attr("class", "equator")
     .attr("clip-path", "url(#clip)")
     .attr("d", path);
-  // Equator Label
-  // svg.append("text").attr("class", "label equator").text("Equator")
-  //                   .attr("x", projection([0, -3])[1])
-  //                   .attr("y", projection([0, 1.5])[1]);
 
   // Capricorn Tropic
   const capricornTropicCoord = { W: -180.0, N: -23.43, E: 180.0, S: -23.43 };
   const capricornTropic = d3.geoGraticule().extentMajor([[capricornTropicCoord.W, capricornTropicCoord.S], [capricornTropicCoord.E, capricornTropicCoord.N]]);
   svg.append("path").attr("class", "capricornTropic").attr("clip-path", "url(#clip)").attr("d", path(capricornTropic.outline()));
-  // Capricorn Tropic Label
-  // svg.append("text").attr("class", "label tropic").text("Capricorn Tropic")
-  //                   .attr("x", projection([0, -10])[1])
-  //                   .attr("y", projection([0, -27])[1]);
 
   // Greenwich Meridian
   const greenwhichCoord = { W: -0.1, N: 90, E: 0.1, S: -90.0 };
   const greenwhich = d3.geoGraticule().extentMajor([[greenwhichCoord.W, greenwhichCoord.S], [greenwhichCoord.E, greenwhichCoord.N]]);
   svg.append("path").attr("class", "greenwhich").attr("clip-path", "url(#clip)").attr("d", path(greenwhich.outline()));
-  // Greenwich Meridian Label
-  // const x1 = projection([0, -42])[1];
-  // const y1 = projection([0, -60])[1];
-  // svg.append("text").attr("class", "label greenwhich").text("Greenwhich Meridian")
-  //                   .attr('transform', 'translate(' + x1 + ', ' + y1 + ') rotate(-90)');
 
   // Display countries
   svg.selectAll(".country")
@@ -145,8 +109,7 @@ const Globe = forwardRef( (props, ref) => {
       .attr("class", "country")
       .attr("clip-path", "url(#clip)")
       .attr("id", d => d.id)
-  // Action
-  // country
+    // Action
     .on("mousemove", function(d,i) {
       d3.select(this).classed('countryHovered', true);
     })
@@ -162,19 +125,11 @@ const Globe = forwardRef( (props, ref) => {
   const arcticCoord = { W: -180.0, N: 90, E: 180.0, S: 66.33 };
   const arcticCircle = d3.geoGraticule().extentMajor([[arcticCoord.W, arcticCoord.S], [arcticCoord.E, arcticCoord.N]]);
   svg.append("path").attr("class", "arcticCircleArea").attr("clip-path", "url(#clip)").attr("d", path(arcticCircle.outline()));
-  // Arctic Circle Label
-  // svg.append("text").attr("class", "label arctic").text("Arctic Circle")
-  //                   .attr("x", projection([0, 10])[1])
-  //                   .attr("y", projection([0, 25])[1]);
                  
   // Antarctic Circle
   const antarcticCoord = { W: -180.0, N: -89.999, E: 180.0, S: -66.33 };
   const antarcticCircle = d3.geoGraticule().extentMajor([[antarcticCoord.W, antarcticCoord.S], [antarcticCoord.E, antarcticCoord.N]]);
   svg.append("path").attr("class", "antarcticCircleArea").attr("clip-path", "url(#clip)").attr("d", path(antarcticCircle.outline()));
-  // Antarctic Circle Label
-  // svg.append("text").attr("class", "label arctic").text("Antarctic Circle")
-  //                   .attr("x", projection([0, 10])[1])
-  //                   .attr("y", projection([0, 25])[1]);
                  
   // Legend and colors
   const quantile = d3.scaleQuantile().domain([
@@ -217,11 +172,8 @@ const Globe = forwardRef( (props, ref) => {
   // Rotate Management
   const resetRotate = () => {
     console.log('resetRotate');
-    //svg.transition().call(drag.rotate([0, 0]), 1);
-    //projection.rotate([0, 0]);
   }
   // Rotation management
-  //console.log("axis", props.mapOptions.rotationX, props.mapOptions.rotationY, props.mapOptions.rotationZ);
   const λ = d3.scaleLinear()
               .domain([0, dimensions.width])
               .range([-180, 180]);
@@ -236,8 +188,6 @@ const Globe = forwardRef( (props, ref) => {
       };
     }).on("drag", function(event) {
       projection.rotate([λ(event.x), φ(event.y)]);
-      //svg.selectAll( ".globe"           ).attr( "d", path );
-      //svg.selectAll( ""       ).attr( "d", path );
       svg.selectAll( ".graticule"       ).attr( "d", path );
       svg.selectAll( ".country"         ).attr( "d", path );
       svg.selectAll( ".arcticCircleArea"    ).attr( "d", path );
@@ -287,8 +237,6 @@ const Globe = forwardRef( (props, ref) => {
   }, []);
 
   useEffect(() => {
-    // Auto resizer
-    //setDimensions( { "width": window.innerWidth, "height": window.innerHeight - 176 } );
     // Handle Rotation (dragging)
     svg.call(dragging);
     // Handle Zooming
